d0b6cde358ae816bcbfeb1217d6b5ba08bdc6d3b
ayukako <yukako1102n@gmail.com>
2022-04-03 14:06:25 +0000

    doing user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,24 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
+    @book = @user.book
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+
+  def update
+    @user = User.find(params[:id])
+    @user.update
+    redirect_to users_show_path
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
 end
