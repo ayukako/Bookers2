a7945ebfe20e553ef6aac8a5774402a9da69b8fb
ayukako <yukako1102n@gmail.com>
2022-04-07 11:05:17 +0000

    done header

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
 
+  validates :name, uniqueness: true
+  validates :introduction, length: { maximum: 50 }
+
+
 has_many :book, dependent: :destroy
 
 
