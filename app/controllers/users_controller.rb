daf41cfd3b0a75766c0faed3e602d242a33063c0
ayukako <yukako1102n@gmail.com>
2022-04-09 13:48:43 +0000

    done User 共通

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,8 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @books = @user.book
+    @user_info = current_user
+    @book = Book.new
   end
 
   def edit
