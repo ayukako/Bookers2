cf46fd0e391fbb3296709eea72f735c604c06473
ayukako <yukako1102n@gmail.com>
2022-04-10 14:03:05 +0000

    almost done

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
 
-  validates :name, uniqueness: true
+  validates :name, uniqueness: true, length: { minimum: 2, maximum: 20 }
   validates :introduction, length: { maximum: 50 }
 
 
